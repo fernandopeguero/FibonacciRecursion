3389fdeb59681ed76a894a2c23a5378c9d1abafe
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-02-16 12:46:34 -0500

    Add logic to create fibonacci sequence using a loop

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,19 @@
-function fibonacci() {}
+function fibonacci(num) {
+    let arr = [0, 1];
 
-function fibsRec() {}
+    for (let i = 2; i < num; i++) {
+        if (arr.length === 0) {
+            arr.push(i);
+        } else {
+            arr.push(arr[i - 2] + arr[i - 1]);
+        }
+    }
+
+    return arr;
+}
+
+console.log(fibonacci(8));
+
+function fibsRec(num) {}
+
+console.log(fibsRec(8));
